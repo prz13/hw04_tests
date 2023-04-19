1dd402e9ac4549f9a22d56e435c2aa894a36b911
Dmitry <36100192+prz13@users.noreply.github.com>
2023-04-19 14:32:34 +0300

    Привел_к_PEP8.

--- yatube/posts/test/test_urls.py
+++ yatube/posts/test/test_urls.py
@@ -35,7 +35,7 @@ class StaticURLTests(TestCase):
         self.authorized_client = Client()
         self.authorized_client.force_login(self.user)
         self.another_user = User.objects.create_user(
-            username='any_user',password='password'
+            username='any_user' ,password='password'
         )
 
     def test_index_and_group(self):
@@ -99,5 +99,3 @@ class StaticURLTests(TestCase):
             response,
             f'/posts/{self.post.id}/'
         )
-
-
