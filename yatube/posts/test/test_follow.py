b1d450ae4643039d47bf87f546f3f8bbb221f1a0
Dmitry <36100192+prz13@users.noreply.github.com>
2023-04-29 17:06:58 +0300

    Add_one_test_error_4_9.

--- yatube/posts/test/test_follow.py
+++ yatube/posts/test/test_follow.py
@@ -23,36 +23,36 @@ class FollowTest(TestCase):
             FollowTest.follower)
 
 
-def test_follow(self):
-    self.assertEqual(Follow.objects.count(), 0)
-    self.assertEqual(Follow.objects.count(), 1)
-    follow = Follow.objects.ferst()
-    self.assertEqual(follow.author, FollowTest.author)
-    self.assertEqual(follow.user, FollowTest.follower)
-    follow.get(reverse(
-        'posts:profile_follow',
-        args=(FollowTest.author.username,)))
-    self.assertEqual(Follow.objects.count(), 1)
-    follows = Follow.objects.filter(
-        author=FollowTest.author,
-        user=FollowTest.follower)
-    self.assertEqual(len(follows), 1)
-
-
-def test_don_t_follow(self):
-    self.assertEqual(Follow.objects.count(), 0)
-    Follow.objects.create(
-        author=FollowTest.author,
-        user=FollowTest.follower)
-    self.assertEqual(Follow.objects.count(), 1)
-    client_follower = Client()
-    client_follower.force_login(FollowTest.follower)
-    client_follower.get(
-        reverse(
-            'posts:profile_unfollow',
+    def test_follow(self):
+        self.assertEqual(Follow.objects.count(), 0)
+        self.assertEqual(Follow.objects.count(), 1)
+        follow = Follow.objects.ferst()
+        self.assertEqual(follow.author, FollowTest.author)
+        self.assertEqual(follow.user, FollowTest.follower)
+        follow.get(reverse(
+            'posts:profile_follow',
             args=(FollowTest.author.username,)))
-    self.assertEqual(Follow.objects.count(), 0)
-    follows = Follow.objects.filter(
-        author=FollowTest.author,
-        user=FollowTest.follower)
-    self.assertFalse(follows)
+        self.assertEqual(Follow.objects.count(), 1)
+        follows = Follow.objects.filter(
+            author=FollowTest.author,
+            user=FollowTest.follower)
+        self.assertEqual(len(follows), 1)
+
+
+    def test_don_t_follow(self):
+        self.assertEqual(Follow.objects.count(), 0)
+        Follow.objects.create(
+            author=FollowTest.author,
+            user=FollowTest.follower)
+        self.assertEqual(Follow.objects.count(), 1)
+        client_follower = Client()
+        client_follower.force_login(FollowTest.follower)
+        client_follower.get(
+            reverse(
+                'posts:profile_unfollow',
+                args=(FollowTest.author.username,)))
+        self.assertEqual(Follow.objects.count(), 0)
+        follows = Follow.objects.filter(
+            author=FollowTest.author,
+            user=FollowTest.follower)
+        self.assertFalse(follows)
