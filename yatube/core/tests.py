8da9bb02609841f903df04a75ff263a67be42384
Dmitry <36100192+prz13@users.noreply.github.com>
2023-04-30 10:35:27 +0300

    Fixed_error_pep8.

--- yatube/core/tests.py
+++ yatube/core/tests.py
@@ -5,9 +5,7 @@ class ViewTestClass(TestCase):
     def setUp(self):
         self.client = Client()
 
-
     def test_page_404(self):
         response = self.client.get('/nonexist-page/')
         self.assertEqual(response.status_code, 404)
         self.assertTemplateUsed(response, 'core/404.html')
-
