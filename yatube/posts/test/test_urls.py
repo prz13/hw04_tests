0b9f31354e118fecded48209268083185b28058b
Dmitry <36100192+prz13@users.noreply.github.com>
2023-04-30 10:27:14 +0300

    Fixed_error_html_404.

--- yatube/posts/test/test_urls.py
+++ yatube/posts/test/test_urls.py
@@ -87,7 +87,7 @@ class StaticURLTests(TestCase):
                 self.assertTemplateUsed(response, template)
 
     def test_page_404(self):
-        response = self.client.get('/unexisting_page/')
+        response = self.client.get('/nonexist-page/')
         self.assertEqual(response.status_code, 404)
         self.assertTemplateUsed(response, 'core/404.html')
 
