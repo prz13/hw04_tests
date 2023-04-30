1384fc7472167dda920297217c9863656c772a93
Dmitry <36100192+prz13@users.noreply.github.com>
2023-04-30 11:17:34 +0300

    Fixed_add_404_403.

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -25,3 +27,11 @@ urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
     path('about/', include('about.urls', namespace='about')),
 ]
+
+handler404 = 'core.views.page_not_found'
+handler403 = 'core.views.csrf_failure'
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
